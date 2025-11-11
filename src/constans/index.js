ed65a3cb48780fdeded1c142bd399efacb56d72d
hisu87 <numpadagain@gmail.com>
2025-11-11 21:03:50 +0700

    UPDATE

--- src/constans/index.js
+++ src/constans/index.js
@@ -31,7 +31,10 @@ import {
   portfolio,
   perfume,
   store,
-  stabuckclone
+  stabuckclone,
+  azure,
+  githubcopilot,
+  nextjs,
 } from "../assets";
 
 export const navLinks = [
@@ -69,6 +72,7 @@ const services = [
 ];
 
 const technologies = [
+  // Front-end
   {
     name: "HTML 5",
     icon: html,
@@ -90,28 +94,66 @@ const technologies = [
     icon: reactjs,
   },
   {
+    name: "Redux",
+    icon: redux,
+  },
+  {
     name: "Tailwind CSS",
     icon: tailwind,
   },
+  {
+    name: "Next.js",
+    icon: nextjs,
+  },
+
+  // Back-end & Databases
   {
     name: "Node JS",
     icon: nodejs,
   },
   {
+    name: "Java",
+    icon: java,
+  },
+  {
+    name: "MongoDB",
+    icon: mongodb,
+  },
+  {
+    name: "Spring Boot",
+    icon: springboot,
+  },
+
+  // Frameworks / Tools / Libraries
+  {
     name: "Three JS",
     icon: threejs,
   },
   {
-    name: "git",
+    name: "Git",
     icon: git,
   },
   {
-    name: "figma",
+    name: "Figma",
     icon: figma,
   },
   {
-    name: "docker",
+    name: "Docker",
     icon: docker,
+  },
+  {
+    name: "GitHub Copilot",
+    icon: githubcopilot,
+  },
+
+  // Cloud
+  {
+    name: "Azure",
+    icon: azure,
+  },
+  {
+    name: "Firebase",
+    icon: firebase,
   },
 ];
 
@@ -126,7 +168,7 @@ const experiences = [
       "Software architecture and design skills.",
       "Basic Java Programming Skills.",
       "Teamwork skill.",
-      "etc..."
+      "etc...",
     ],
   },
   {
@@ -172,6 +214,148 @@ const testimonials = [
 ];
 
 const projects = [
+  {
+    name: "LinkHub87",
+    description:
+      "Một nền tảng quản lý liên kết hiện đại, hiệu suất cao với phân tích nâng cao, theo dõi thời gian thực và bộ đệm thông minh.",
+    tags: [
+      {
+        name: "next.js",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "react",
+        color: "green-text-gradient",
+      },
+      {
+        name: "typescript",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "firebase",
+        color: "orange-text-gradient",
+      },
+      {
+        name: "tailwind",
+        color: "blue-text-gradient",
+      },
+    ],
+    image: "your_image_variable_for_linkhub87", // Cần thay thế bằng biến hình ảnh của bạn
+    source_code_link: "https://github.com/hisu87/linkhub87",
+  },
+  {
+    name: "Ecommerce Fragnant Store",
+    description:
+      "An ecommerce website for selling fragnant with admin page to management product, revenuew, employee.",
+    tags: [
+      {
+        name: "JSP",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "Servlet",
+        color: "green-text-gradient",
+      },
+      {
+        name: "Java",
+        color: "orange-text-gradient",
+      },
+      {
+        name: "MS SQL Server",
+        color: "yellow-text-gradient",
+      },
+    ],
+    image: "your_image_variable_for_webtmdt", // Cần thay thế bằng biến hình ảnh của bạn
+    source_code_link: "https://github.com/hisu87/webTMDT",
+  },
+  {
+    name: "Project1",
+    description:
+      "Không tìm thấy mô tả chi tiết cho dự án này. Bạn nên cập nhật mô tả.",
+    tags: [],
+    image: "your_image_variable_for_project1", // Cần thay thế bằng biến hình ảnh của bạn
+    source_code_link: "https://github.com/hisu87/Project1",
+  },
+  {
+    name: "SOF2041",
+    description: "Dự án mẫu môn SOF2041 9/2023.",
+    tags: [
+      {
+        name: "java",
+        color: "blue-text-gradient",
+      },
+    ],
+    image: "your_image_variable_for_sof2041", // Cần thay thế bằng biến hình ảnh của bạn
+    source_code_link: "https://github.com/hisu87/SOF2041",
+  },
+  {
+    name: "Spotify Clone",
+    description:
+      "A Spotify clone that allows users to search for songs, create playlists, and play music, upload songs and img using supabase for data, and stripe for subsciption.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "supabase",
+        color: "green-text-gradient",
+      },
+      {
+        name: "stripe",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: "your_image_variable_for_spotify", // Cần thay thế bằng biến hình ảnh của bạn
+    source_code_link: "https://github.com/hisu87/spotify-clone",
+  },
+  {
+    name: "Starbucks Clone Remake",
+    description:
+      "A Starbucks clone with different UI/UX and more creative. By using HTML, CSS and JavaScript. For a new experience.",
+    tags: [
+      {
+        name: "HTML",
+        color: "green-text-gradient",
+      },
+      {
+        name: "CSS",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "JavaScript",
+        color: "orange-text-gradient",
+      },
+    ],
+    image: "your_image_variable_for_starbucks", // Cần thay thế bằng biến hình ảnh của bạn
+    source_code_link: "https://github.com/hisu87/Web-Starbuck-Clone-Remake",
+  },
+  {
+    name: "Ecommerce Store",
+    description:
+      "An ecommerce website for selling fragnant with admin page to management product, revenuew, employee",
+    tags: [
+      {
+        name: "Spring Boot",
+        color: "green-text-gradient",
+      },
+      {
+        name: "Thymeleaf",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "JavaScript",
+        color: "orange-text-gradient",
+      },
+      {
+        name: "MS SQL Server",
+        color: "yellow-text-gradient",
+      },
+    ],
+    image: "your_image_variable_for_sof306", // Cần thay thế bằng biến hình ảnh của bạn
+    source_code_link: "https://github.com/hisu87/SOF306-Java6-ASM",
+  },
+
   {
     name: "3D Portfolio",
     description:
@@ -225,7 +409,8 @@ const projects = [
       },
     ],
     image: java,
-    source_code_link: "https://github.com/hisu87/PS33100-PhamVanHieu-Assignment-Java2",
+    source_code_link:
+      "https://github.com/hisu87/PS33100-PhamVanHieu-Assignment-Java2",
   },
   {
     name: "Ecommerce Fragnant Store",
@@ -235,7 +420,8 @@ const projects = [
       {
         name: "JSP",
         color: "blue-text-gradient",
-      }, {
+      },
+      {
         name: "Servlet",
         color: "green-text-gradient",
       },
@@ -259,7 +445,8 @@ const projects = [
       {
         name: "Spring Boot",
         color: "green-text-gradient",
-      }, {
+      },
+      {
         name: "Thymeleaf",
         color: "green-text-gradient",
       },
@@ -283,21 +470,19 @@ const projects = [
       {
         name: "HTML",
         color: "green-text-gradient",
-      }, {
+      },
+      {
         name: "CSS",
         color: "green-text-gradient",
       },
       {
         name: "JavaScript",
         color: "orange-text-gradient",
-      }
-
+      },
     ],
     image: stabuckclone,
     source_code_link: "https://github.com/hisu87/SOF306-Java6-ASM",
   },
-
-
 ];
 
 export { services, technologies, experiences, testimonials, projects };
